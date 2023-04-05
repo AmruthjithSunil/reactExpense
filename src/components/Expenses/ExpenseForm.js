b47b95f52ad346f8ae897dc1c0335a0a9ba94f9d
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-04-05 21:52:01 +0530

    advance react task 13

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -13,6 +13,7 @@ const ExpenseForm = ({ onExpenseSave }) => {
     setEnteredAmount("");
     setEnteredDate("");
     setEnteredTitle("");
+    setReveal(false);
   };
 
   const changeTitleHandler = (e) => {
@@ -27,41 +28,57 @@ const ExpenseForm = ({ onExpenseSave }) => {
     setEnteredDate(e.target.value);
   };
 
+  const [reveal, setReveal] = useState(false);
   const [enteredTitle, setEnteredTitle] = useState();
   const [enteredAmount, setEnteredAmount] = useState();
   const [enteredDate, setEnteredDate] = useState();
 
-  return (
-    <>
-      <form onSubmit={submitHandler}>
-        <label for="title">Expense Title:</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={enteredTitle}
-          onChange={changeTitleHandler}
-        ></input>
-        <label for="amount">Expense Amount:</label>
-        <input
-          type="number"
-          id="amount"
-          name="amount"
-          value={enteredAmount}
-          onChange={changeAmountHandler}
-        ></input>
-        <label for="date">Date:</label>
-        <input
-          type="date"
-          id="date"
-          name="date"
-          value={enteredDate}
-          onChange={changeDateHandler}
-        ></input>
-        <button type="submit">Add Expense</button>
-      </form>
-    </>
+  const content = reveal ? (
+    <form onSubmit={submitHandler}>
+      <label for="title">Expense Title:</label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={enteredTitle}
+        onChange={changeTitleHandler}
+      ></input>
+      <label for="amount">Expense Amount:</label>
+      <input
+        type="number"
+        id="amount"
+        name="amount"
+        value={enteredAmount}
+        onChange={changeAmountHandler}
+      ></input>
+      <label for="date">Date:</label>
+      <input
+        type="date"
+        id="date"
+        name="date"
+        value={enteredDate}
+        onChange={changeDateHandler}
+      ></input>
+      <button
+        onClick={() => {
+          setReveal(false);
+        }}
+      >
+        Cancel
+      </button>
+      <button type="submit">Add Expense</button>
+    </form>
+  ) : (
+    <button
+      onClick={() => {
+        setReveal(true);
+      }}
+    >
+      Add Expense
+    </button>
   );
+
+  return <div>{content}</div>;
 };
 
 export default ExpenseForm;
