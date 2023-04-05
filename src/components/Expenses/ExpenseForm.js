919d5942f661b68a0395c146571bdb97b8e40a5d
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-04-05 14:41:42 +0530

    advance react task 4

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,18 +1,53 @@
+import { useState } from "react";
+import "./ExpenseForm.css";
+
 const ExpenseForm = () => {
-  const changeHandler = (e) => {
-    console.log(e.nativeEvent.data);
+  const changeTitleHandler = (e) => {
+    setEnteredTitle(e.target.value);
   };
 
+  const changeAmountHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const changeDateHandler = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
+  const [enteredTitle, setEnteredTitle] = useState();
+  const [enteredAmount, setEnteredAmount] = useState();
+  const [enteredDate, setEnteredDate] = useState();
+
   return (
-    <form onChange={changeHandler}>
-      <label for="title">Expense Title:</label>
-      <input type="text" id="title" name="title"></input>
-      <label for="amount">Expense Amount:</label>
-      <input type="number" id="amount" name="amount"></input>
-      <label for="date">Date:</label>
-      <input type="date" id="date" name="date"></input>
-      <button type="submit">Add Expense</button>
-    </form>
+    <>
+      <form>
+        <label for="title">Expense Title:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={changeTitleHandler}
+        ></input>
+        <label for="amount">Expense Amount:</label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          onChange={changeAmountHandler}
+        ></input>
+        <label for="date">Date:</label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          onChange={changeDateHandler}
+        ></input>
+        <button type="submit">Add Expense</button>
+      </form>
+      <p>
+        {enteredTitle} {enteredAmount} {enteredDate}
+      </p>
+    </>
   );
 };
 
