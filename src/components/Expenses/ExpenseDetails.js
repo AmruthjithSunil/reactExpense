1eec76fc27acb1ca2943534751707c93d4844d07
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-04-05 10:06:27 +0530

    advance react task 1

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,6 +1,14 @@
 import "./ExpenseItem.css";
 
 function ExpenseDetails({ locationOfExpenditure, title, amount }) {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
+
+  const deleteHandler = (e) => {
+    e.target.parentElement.remove();
+  };
+
   return (
     <>
       <div>{locationOfExpenditure}</div>
@@ -8,6 +16,8 @@ function ExpenseDetails({ locationOfExpenditure, title, amount }) {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete Expense</button>
     </>
   );
 }
